3c2672ca6f3b5452ed3939a3d277176f97ca162f
Shiv <ss4328@drexel.edu>
2021-05-17 05:00:03 -0400

    content Deletion integrated, authentication check added

--- CourseMatter/content/urls.py
+++ CourseMatter/content/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import AddContentView, UpdateContentView
+from .views import AddContentView, UpdateContentView, DeleteContentView
 
 urlpatterns = [
     path('', views.ContentList.as_view(), name='index'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('content_detail/<slug:slug>/', views.ContentDetail.as_view(), name='content_detail'),
     path('tags/<slug:tag_slug>/', views.TagListView.as_view(), name='content_by_tag'),
     path('add_content', AddContentView.as_view(), name="add_content"),
-    path('content/edit/<slug:slug>', UpdateContentView.as_view(), name="edit_content")
+    path('content/edit/<slug:slug>', UpdateContentView.as_view(), name="edit_content"),
+    path('content/delete/<slug:slug>', DeleteContentView.as_view(), name="delete_content"),
 ]
--- CourseMatter/content/views.py
+++ CourseMatter/content/views.py
@@ -1,17 +1,20 @@
 from django.views import generic
 from .models import Content
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # from .forms import ContentForm
+from django.urls import reverse_lazy
 
-class ContentList(generic.ListView):
+
+class ContentList(ListView):
     queryset = Content.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 4
 
 
-class ContentDetail(generic.DetailView):
+class ContentDetail(DetailView):
     model = Content
     template_name = 'content_detail.html'
+
 
 class AddContentView(CreateView):
     model = Content
@@ -19,15 +22,25 @@ class AddContentView(CreateView):
     template_name = 'add_content.html'
     fields = '__all__'
 
+
 class UpdateContentView(UpdateView):
     model = Content
     # form_class = ContentForm
     template_name = 'update_content.html'
     fields = '__all__'
 
+
+class DeleteContentView(DeleteView):
+    model = Content
+    template_name = 'delete_content.html'
+    success_url = reverse_lazy('login')
+
+
 '''
-Returns view with all posts for a single tag
+Returns view with all content for a single tag
 '''
+
+
 class TagListView(ListView):
     template_name = "tag_contents.html"
 
